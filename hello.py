df07c9178bf1c12ab3b054c673f232a2434f171a
Ishii <laafan@DESKTOP-CF7P2RJ.localdomain>
2025-09-04 01:32:55 +0900

    メッセージを変更

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import json, urllib.request
 url = "https://hooks.slack.com/services/XXXX/XXXX/XXXXXXXX"
-data = {"text": "こんにちは. お元気ですか?"}
+data = {"text": "Hello World!"}
 req = urllib.request.Request(url, data=json.dumps(data).encode(), headers={"Content-Type":"application/json"})
 urllib.request.urlopen(req)
